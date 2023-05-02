a3d7355a27f28063c0d38732bfeb566524612453
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-05-02 16:09:15 +0600

    2nd commit set url and data fatch success

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Chef data is working...')
 })
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
+})
+
+// app.get('/chef/:id', (req, res) => {
+//     const id = req.params.id;
+//     console.log(id)
+//     const selectChef = chef.find(c => c.id == id);
+//     res.send(selectChef);
+//   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
