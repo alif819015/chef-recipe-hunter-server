3ca8e313457d8d4916aedde2ff6a1bdc4f5588a0
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-05-03 10:02:15 +0600

    3rd commit json data find

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.get('/chef', (req, res) => {
   res.send(chef);
 })
 
-// app.get('/chef/:id', (req, res) => {
-//     const id = req.params.id;
-//     console.log(id)
-//     const selectChef = chef.find(c => c.id == id);
-//     res.send(selectChef);
-//   })
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectChef = chef.find(c => c.id == id);
+    res.send(selectChef);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
